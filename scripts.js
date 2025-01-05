6538c366116bf54db33a30209afcb9519017f255
Stealr <baranovm003@yandex.ru>
2025-01-05 16:53:34 +0300

    Адаптирован слайдер на мольную версию

--- scripts.js
+++ scripts.js
@@ -1,22 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Инициализация слайдера миниатюр
-    const thumbnailSwiper = new Swiper('.thumbnail-slider', {
-      spaceBetween: 10,
-      slidesPerView: 5.3,
-      freeMode: true,
-      watchSlidesProgress: true,
-    });
-  
-    // Инициализация основного слайдера
-    const mainSwiper = new Swiper('.main-slider', {
-      spaceBetween: 0,
-      navigation: {
-        nextEl: '.swiper-button.next',
-        prevEl: '.swiper-button.prev',
+  // Инициализация слайдера миниатюр
+  var thumbnailSwiper = new Swiper('.thumbnail-slider', {
+    breakpoints: {
+      400: {
+        slidesPerView: 1.2,
       },
-      thumbs: {
-        swiper: thumbnailSwiper, // Привязка миниатюр
+      500: {
+        slidesPerView: 1.5,
       },
-    });
+      650: {
+        slidesPerView: 2,
+      },
+      750: {
+        slidesPerView: 2.3,
+      },
+      1070: {
+        slidesPerView: 3.3,
+      },
+      1385: {
+        slidesPerView: 4.3,
+      },
+      1700: {
+        slidesPerView: 5.3,
+      },
+    },
+    freeMode: true,
+    watchSlidesProgress: true,
   });
-  
+
+  // Инициализация основного слайдера
+  var mainSwiper = new Swiper('.main-slider', {
+    spaceBetween: 0,
+    navigation: {
+      nextEl: '.swiper-button.next',
+      prevEl: '.swiper-button.prev',
+    },
+    pagination: {
+      el: '.swiper-pagination', // Селектор для контейнера точек
+      clickable: true, // Делает точки кликабельными
+    },
+    thumbs: {
+      swiper: thumbnailSwiper, // Привязка миниатюр
+    },
+  });
+});
